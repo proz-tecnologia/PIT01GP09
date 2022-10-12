3952a4fec0eae435571babb1aee48567be4ab635
cindi_star <cindi_nishi@hotmail.com>
2022-10-11 23:25:10 -0300

    inserido o nome Cindi na main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test',
+      title: 'Test Cindi',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
