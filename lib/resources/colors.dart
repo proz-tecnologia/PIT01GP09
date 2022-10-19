8d01c57ef9ce566ef8902ec9c5a5e32db5103ac4
cindi_star <cindi_nishi@hotmail.com>
2022-10-18 21:04:21 -0300

    adicionada a home page e design system

---
+++ lib/resources/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purpleFlower = Color(0xFF6A49FA);
+  static const blueLight = Color(0xFF6FE5FF);
+  static const yellowOne = Color(0xFFFAFF00);
+  static const blueVibrant = Color(0xFF05EDE3);
+  static const amberPeach = Color(0xFFFFD600);
+  static const greenVibrant = Color(0xFF3CF3A6);
+  static const redWine = Color(0xFFFF4F4F);
+  static const grayLight = Color(0xFFEDEDED);
+  static const grayDark = Color(0xFF949393);
+  static const blackSwan = Color(0xFF000000);
+  static const grayTwo = Color(0xFFAEAEAE);
+  static const graySuperLight = Color(0xFFF5F5F5);
+  static const whiteSnow = Color(0xFFFFFFFF);
+}
---
+++ lib/resources/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+  static const appName = 'Finance App';
+  static const income = 'Receitas';
+  static const expenses = 'Despesas';
+}
