845d8295be9d83aa2875142637428a5ffa9814dc
cindi_star <cindi_nishi@hotmail.com>
2022-10-18 22:23:47 -0300

    mudei a flag para false

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: Strings.appName,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
