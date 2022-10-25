eeac55461332946d580336f5b81ebf043149174c
cindi_star <cindi_nishi@hotmail.com>
2022-10-25 01:08:05 -0300

    ajustado o profile removendo o circleavatar

--- lib/presentation/home/widgets/profile.dart
+++ lib/presentation/home/widgets/profile.dart
@@ -11,20 +11,19 @@ class Profile extends StatelessWidget {
       width: 65,
       height: 65,
       decoration: BoxDecoration(
-//color: Colors.white,
         shape: BoxShape.circle,
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 0,
-            blurRadius: 4,
-            offset: const Offset(1, 4), // changes position of shadow
+            blurRadius: 30,
+            blurStyle: BlurStyle.normal,
+            offset: const Offset(1, 4),
           ),
         ],
       ),
-      child: CircleAvatar(
-        radius: 65,
-        child: Image.asset('assets/images/user-pic.png'),
+      child: Image.asset(
+        'assets/images/user-pic.png',
       ),
     );
   }
