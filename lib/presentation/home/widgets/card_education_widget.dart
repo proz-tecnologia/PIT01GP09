d39ecf1b537d6f486fbf72c8a7fb5a6864ab506e
cindi_star <cindi_nishi@hotmail.com>
2022-10-25 21:53:23 -0300

    ajustados os detalhes da fonte do text 'educação financeira'

--- lib/presentation/home/widgets/card_education_widget.dart
+++ lib/presentation/home/widgets/card_education_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test/resources/colors.dart';
 
-import '../../../resources/colors.dart';
 import '../../../resources/strings.dart';
 
 class CardEducationWidget extends StatefulWidget {
@@ -14,10 +14,18 @@ class _CardEducationWidgetState extends State<CardEducationWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(left: 16, top: 20),
       height: MediaQuery.of(context).size.height * 0.30,
       width: MediaQuery.of(context).size.width,
-      color: AppColors.graySuperLight,
-      child: const Text(Strings.financialEducation),
+      color: AppColors.amberPeach,
+      child: const Text(
+        Strings.financialEducation,
+        style: TextStyle(
+          color: AppColors.blackSwan,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
     );
   }
 }
