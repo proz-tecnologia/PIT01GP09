36631aa8229487d36258ca113ab7e0435c9cd985
cindi_star <cindi_nishi@hotmail.com>
2022-10-25 21:51:10 -0300

    removido o pageview e ajustados os widgets customizados e os itens da bottomnavigationbar

--- lib/presentation/home/page/home_page.dart
+++ lib/presentation/home/page/home_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:test/presentation/home/widgets/card_chart_widget.dart';
 import 'package:test/presentation/home/widgets/card_education_widget.dart';
+import 'package:test/presentation/home/widgets/card_financial_statement_widget.dart';
 import 'package:test/presentation/home/widgets/card_gradient_widget.dart';
-import 'package:test/resources/my_images.dart';
-
-import '../../figma/home_page_figma.dart';
-import '../widgets/card_chart_widget.dart';
-import '../widgets/card_financial_statement_widget.dart';
-import 'homeT.dart';
+import 'package:test/resources/colors.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -18,33 +15,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentPage = 0;
-  late PageController pc;
+  int currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    pc = PageController(initialPage: currentPage);
-  }
-
-  setPaginaAtual(pagina) {
+  void onItemPressed(int index) {
     setState(() {
-      currentPage = pagina;
+      currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
-      body: PageView(
-        // controller: pc,
-        children: [
-          ListView(
-            children: [
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.graySuperLight,
+        body: Center(
+          child: ListView(
+            children: <Widget>[
               Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   CardGradientWidget(),
                   CardFinancialStatementWidget(),
@@ -54,44 +42,29 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-          const PrintHomePageFigma(),
-          Home(),
-        ],
-        onPageChanged: (value) {
-          print('ir para função que atualiza staus da pagina');
-          // currentPage =
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed('/help_home');
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.help),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentPage,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.money), label: 'Transações'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance), label: 'Banco'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_wallet), label: 'Carteira'),
-          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'Mais'),
-          // const BottomNavigationBarItem(
-          //     icon: Icon(Icons.star), label: 'Favorito'),
-          // const BottomNavigationBarItem(
-          //     icon: Icon(Icons.settings), label: 'Configurações'),
-        ],
-        onTap: (pagina) {
-          pc.animateToPage(
-            pagina,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.ease,
-          );
-        },
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: AppColors.blueVibrant,
+          onPressed: () {},
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: AppColors.blueVibrant,
+          unselectedItemColor: AppColors.grayTwo,
+          currentIndex: currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.currency_exchange), label: 'Transações'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_balance), label: 'Bancos'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.more_horiz), label: 'Mais'),
+          ],
+          onTap: onItemPressed,
+        ),
       ),
     );
   }
