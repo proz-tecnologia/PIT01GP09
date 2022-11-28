48bbf59942ec6c8e6e707a0c4f03c8f3161db381
cindi_star <cindi_nishi@hotmail.com>
2022-11-27 23:33:58 -0300

    criado o widget de bottom sheet para transações

---
+++ lib/presentation/transactions/widgets/bottom_sheet_transactions_widget.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:test/resources/colors.dart';
+
+class BottomSheetTransactionsWidget extends StatelessWidget {
+  const BottomSheetTransactionsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 72,
+      width: double.maxFinite,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              color: AppColors.grayLight,
+              child: Column(
+                children: const [
+                  SizedBox(height: 20),
+                  Text(
+                    'R\$ 55.00',
+                    style: TextStyle(
+                      color: AppColors.greenVibrant,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Receitas',
+                    style: TextStyle(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: AppColors.graySuperLight,
+              child: Column(
+                children: const [
+                  SizedBox(height: 20),
+                  Text(
+                    'R\$ 5.000,00',
+                    style: TextStyle(
+                      color: AppColors.purpleFlower,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Saldo',
+                    style: TextStyle(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: AppColors.grayLight,
+              child: Column(
+                children: const [
+                  SizedBox(height: 20),
+                  Text(
+                    'R\$ 600,00',
+                    style: TextStyle(
+                      color: AppColors.redWine,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Despesas',
+                    style: TextStyle(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
