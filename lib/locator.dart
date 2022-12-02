dbe09ffc5bcd6b0e948fc7cd435e4cdaeb142d49
cindi_star <cindi_nishi@hotmail.com>
2022-12-02 01:03:45 -0300

    adicionados novos controllers

--- lib/locator.dart
+++ lib/locator.dart
@@ -3,11 +3,19 @@ import 'package:get_it/get_it.dart';
 
 import 'data/repositories/transaction_repository_impl.dart';
 import 'domain/repositories/transaction_repository.dart';
+import 'presentation/expenses/controller/expenses_controller.dart';
 import 'presentation/home/controller/transaction_controller.dart';
+import 'presentation/income/controller/income_controller.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
+  getIt.registerLazySingleton<ExpensesController>(
+      () => ExpensesController(transactionRepository: getIt()));
+
+  getIt.registerLazySingleton<IncomeController>(
+      () => IncomeController(transactionRepository: getIt()));
+
   getIt.registerLazySingleton<TransactionController>(
       () => TransactionController(transactionRepository: getIt()));
 
