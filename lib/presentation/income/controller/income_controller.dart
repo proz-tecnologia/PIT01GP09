bda1fcebc80bc30cfcb396562ccc2badadccdcf9
cindi_star <cindi_nishi@hotmail.com>
2022-12-02 01:08:20 -0300

    criado o IncomeController

---
+++ lib/presentation/income/controller/income_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:test/domain/repositories/transaction_repository.dart';
+import 'package:test/presentation/income/controller/income_state.dart';
+
+class IncomeController extends ValueNotifier<IncomeState> {
+  final TransactionRepository _transactionRepository;
+
+  IncomeController({
+    required TransactionRepository transactionRepository,
+  })  : _transactionRepository = transactionRepository,
+        super(IncomeInitialState());
+
+  Future<void> getIncomeTransactionList() async {
+    value = IncomeLoadingState();
+
+    try {
+      var incomeTransactionList = await _transactionRepository.getTransactionList();
+      var incomefilteredList = incomeTransactionList.where((item) => item.type == 'Receita').toList();
+
+      value = IncomeSuccessState(incomefilteredList);
+    } catch (e) {
+      value = IncomeErrorState(e.toString());
+    }
+  }
+}
