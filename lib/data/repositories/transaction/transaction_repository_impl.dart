ac14da5fcda72a7226cf6c36489df2110faf4668
cindi_star <cindi_nishi@hotmail.com>
2023-01-09 02:20:11 -0300

    ajustada a indentação da linha

---
+++ lib/data/repositories/transaction/transaction_repository_impl.dart
@@ -0,0 +1,79 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+import '../../../domain/repositories/transaction/transaction_repository.dart';
+import '../../models/transaction_model.dart';
+
+class TransactionRepositoryImpl implements TransactionRepository {
+  final Dio _dio;
+
+  TransactionRepositoryImpl(this._dio);
+
+
+  final _baseUrl = 'https://crudcrud.com/api/a8c16e04e05e4034bd062802bb846aaf';
+
+  @override
+  Future<List<TransactionModel>> getTransactionList() async {
+    final Response response = await _dio.get(
+      '$_baseUrl/transactions',
+    );
+    final list = List.from(response.data);
+    final result = list.map((e) => TransactionModel.fromMap(e)).toList();
+    log('list $result');
+    return result;
+  }
+
+  @override
+  Future<TransactionModel> createTransaction(
+      TransactionModel transactionModel) async {
+    final Response response = await _dio.post(
+      '$_baseUrl/transactions',
+      data: {
+        'description': transactionModel.description,
+        'category': transactionModel.category,
+        'type': transactionModel.type,
+        'value': transactionModel.value,
+      },
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      ),
+    );
+    log('create $response');
+    final todos = TransactionModel.fromMap(response.data);
+    return todos;
+  }
+
+  @override
+  Future<void> updateTransaction(TransactionModel transactionModel) async {
+    final response = await _dio.put(
+      '$_baseUrl/transactions/${transactionModel.id}',
+      data: {
+        'description': transactionModel.description,
+        'category': transactionModel.category,
+        'type': transactionModel.type,
+        'value': transactionModel.value,
+      },
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      ),
+    );
+    response;
+    log('update ${response.statusCode}');
+  }
+
+  @override
+  Future<void> deleteTransaction(String transactionId) async {
+    final response = await _dio.delete(
+      '$_baseUrl/transactions/$transactionId',
+      options: Options(
+        headers: <String, String>{'Content-Type': 'text/plain'},
+      ),
+    );
+    log('delete $response');
+  }
+}
