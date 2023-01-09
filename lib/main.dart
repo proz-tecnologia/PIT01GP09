759d31f79086a28f5e18d62b424e75717e9d1f42
cindi_star <cindi_nishi@hotmail.com>
2023-01-09 02:14:53 -0300

    incluído o Firebase.initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,37 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:test/locator.dart';
-import 'data/models/transaction_model.dart';
-import 'data/repositories/transaction_repository_impl.dart';
-import 'locator.dart' as locator;
 
+import 'data/repositories/transaction/transaction_repository_impl.dart';
+import 'firebase_options.dart';
+import 'locator.dart';
+import 'locator.dart' as locator;
 import 'my_app.dart';
 import 'presentation/expenses/controller/expenses_controller.dart';
 import 'presentation/home/controller/transaction_controller.dart';
 import 'presentation/income/controller/income_controller.dart';
+import 'presentation/login/controller/auth_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   locator.setup();
   final Dio dio = Dio();
   final httpClient = TransactionRepositoryImpl(dio);
 
-  await httpClient.createTransaction(
-    TransactionModel(
-      description: 'Herança de família',
-      category: 'Herança',
-      type: 'Receita',
-      value: 9000.0,
-      date: DateTime.now(),
-    ),
-  );
+  // await httpClient.createTransaction(
+  //   TransactionModel(
+  //     description: 'Herança de família',
+  //     category: 'Herança',
+  //     type: 'Receita',
+  //     value: 9000.0,
+  //     date: DateTime.now(),
+  //   ),
+  // );
 
   // await httpClient.getTransactionList();
 
@@ -43,6 +48,11 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthController(
+            authRepository: getIt(),
+          ),
+        ),
         ChangeNotifierProvider(
           create: (_) => ExpensesController(
             transactionRepository: getIt(),
