090457d41f99c49412347881c6a4f07388b778ab
cindi_star <cindi_nishi@hotmail.com>
2023-01-12 19:38:21 -0300

    ajustado para receber a lista do firestore

--- lib/presentation/income/controller/income_controller.dart
+++ lib/presentation/income/controller/income_controller.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:test/domain/repositories/transaction/transaction_repository.dart';
+import 'package:test/domain/repositories/authentication/auth_repository.dart';
+import 'package:test/domain/repositories/transactions/transactions_repository.dart';
 import 'package:test/presentation/income/controller/income_state.dart';
 
 class IncomeController extends ValueNotifier<IncomeState> {
-  final TransactionRepository _transactionRepository;
+  final TransactionsRepository _transactionsRepository;
+  final AuthRepository _authRepository;
+
+  final notifier = ValueNotifier<IncomeState>(IncomeInitialState());
+
+  IncomeState get state => notifier.value;
 
   IncomeController({
-    required TransactionRepository transactionRepository,
-  })  : _transactionRepository = transactionRepository,
+    required TransactionsRepository transactionsRepository,
+    required AuthRepository authRepository,
+  })  : _transactionsRepository = transactionsRepository,
+        _authRepository = authRepository,
         super(IncomeInitialState());
 
   Future<void> getIncomeTransactionList() async {
-    value = IncomeLoadingState();
+    notifier.value = IncomeLoadingState();
 
     try {
-      var incomeTransactionList = await _transactionRepository.getTransactionList();
-      var incomefilteredList = incomeTransactionList.where((item) => item.type == 'Receita').toList();
+      final userId = _authRepository.currentUser?.uid;
 
-      value = IncomeSuccessState(incomefilteredList);
+      var incomeTransactionsList =
+          await _transactionsRepository.getTransactionsList(userId ?? "");
+      var incomefilteredList = incomeTransactionsList
+          .where((item) => item.type == 'Receita')
+          .toList();
+
+      notifier.value = IncomeSuccessState(incomefilteredList);
     } catch (e) {
-      value = IncomeErrorState(e.toString());
+      notifier.value = IncomeErrorState(e.toString());
     }
   }
 }
