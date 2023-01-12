40460f765cf055796a6fd4779046a86baab2e696
cindi_star <cindi_nishi@hotmail.com>
2023-01-12 19:39:36 -0300

    criada a SplashController

---
+++ lib/presentation/splash/controller/splash_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:test/domain/repositories/authentication/auth_repository.dart';
+import 'package:test/presentation/splash/controller/splash_state.dart';
+
+class SplashController extends ValueNotifier<SplashState> {
+  final AuthRepository _authRepository;
+
+  final notifier = ValueNotifier<SplashState>(SplashLoading());
+
+  SplashController({
+    required AuthRepository authRepository,
+  })  : _authRepository = authRepository,
+        super(SplashLoading());
+
+  SplashState get state => notifier.value;
+
+  Future<void> getUser() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (_authRepository.isLogged) {
+      notifier.value = SplashAuthenticated();
+    } else {
+      notifier.value = SplashUnAuthenticated();
+    }
+  }
+}
