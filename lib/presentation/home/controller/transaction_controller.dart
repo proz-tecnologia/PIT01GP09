f8727a708da25db1cec5f29375765304a448baf3
cindi_star <cindi_nishi@hotmail.com>
2023-01-12 19:38:07 -0300

    ajustado para receber a lista do firestore

--- lib/presentation/home/controller/transaction_controller.dart
+++ lib/presentation/home/controller/transaction_controller.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:test/domain/repositories/transaction/transaction_repository.dart';
+import 'package:test/domain/repositories/authentication/auth_repository.dart';
+import 'package:test/domain/repositories/transactions/transactions_repository.dart';
 import 'package:test/presentation/home/controller/transaction_state.dart';
 
-class TransactionController extends ValueNotifier<TransactionState> {
-  final TransactionRepository _transactionRepository;
+class TransactionsController extends ValueNotifier<TransactionState> {
+  final TransactionsRepository _transactionsRepository;
+  final AuthRepository _authRepository;
 
-  TransactionController({
-    required TransactionRepository transactionRepository,
-  })  : _transactionRepository = transactionRepository,
-        super(TransactionInitialState());
+  TransactionsController({
+    required TransactionsRepository transactionsRepository,
+    required AuthRepository authRepository,
+  })  : _transactionsRepository = transactionsRepository,
+        _authRepository = authRepository,
+        super(
+          TransactionInitialState());
 
-  Future<void> getTransactionList() async {
+  Future<void> getTransactionsList() async {
     value = TransactionLoadingState();
 
     try {
-      final transactionList = await _transactionRepository.getTransactionList();
-      value = TransactionSuccessState(transactionList);
+      final userId = _authRepository.currentUser?.uid;
+
+      final transactionsList =
+          await _transactionsRepository.getTransactionsList(userId ?? "");
+
+      value = TransactionSuccessState(transactionsList);
     } catch (e) {
       value = TransactionErrorState(e.toString());
     }
