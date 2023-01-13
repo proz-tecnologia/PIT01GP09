ebf162e0f9a42f3fc7437bd85673870c25183917
cindi_star <cindi_nishi@hotmail.com>
2023-01-13 00:00:07 -0300

    criada a extensão DateTimeUtils

--- lib/utils/date_util.dart
+++ lib/utils/date_util.dart
@@ -30,12 +30,3 @@ extension DateTimeUtils on DateTime {
     }
   }
 }
-
-class DateTimeModel {
-  final DateTime? date;
-  DateTimeModel({
-    required this.date,
-  });
-  String get name =>
-      date == null ? 'Todos' : '${date!.getMonthIntoString()}/${date!.year}';
-}
