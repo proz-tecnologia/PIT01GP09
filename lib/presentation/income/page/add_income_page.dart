bc50957ef04881788b067bf0a68961fcb60926b7
cindi_star <cindi_nishi@hotmail.com>
2023-01-17 00:07:17 -0300

    list renomeada para listIncome

--- lib/presentation/income/page/add_income_page.dart
+++ lib/presentation/income/page/add_income_page.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:test/presentation/add/page/add_page.dart';
+import 'package:test/resources/shared_widgets/add_new_transaction_widget.dart';
 import 'package:test/resources/colors.dart';
+import 'package:test/utils/list_income_util.dart';
 
 import '../../../resources/strings.dart';
-
-const List<String> list = <String>[
-  "",
-  "Salário",
-  "Renda",
-  "Venda",
-  "Freelance",
-  "Outras receitas",
-];
 
 class AddIncomePage extends StatefulWidget {
   const AddIncomePage({super.key});
@@ -21,17 +13,13 @@ class AddIncomePage extends StatefulWidget {
 }
 
 class _AddIncomePageState extends State<AddIncomePage> {
-  get color => AppColors.greenVibrant;
-
-  get type => Strings.income;
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: AddPage(
-      color: color,
-      type: type,
-      list: list,
+    return const Center(
+        child: AddNewTransactionWidget(
+      color: AppColors.greenVibrant,
+      type: Strings.income,
+      list: listIncome,
     ));
   }
 }
