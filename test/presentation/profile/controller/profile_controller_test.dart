464a0e5c19c5566be2fa1ac10f7d4124ba09fc55
josemailson <jmailsons@gmail.com>
2023-01-22 16:42:43 -0300

    teste para profile controller

---
+++ test/presentation/profile/controller/profile_controller_test.dart
@@ -0,0 +1,40 @@
+import 'package:finance_app/data/repositories/authentication/auth_repository_impl.dart';
+import 'package:finance_app/presentation/login/controller/auth_controller.dart';
+import 'package:finance_app/presentation/profile/controller/profile_controller.dart';
+import 'package:finance_app/presentation/profile/controller/profile_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../mock/mock_classes.dart';
+
+void main() {
+  late AuthRepositoryImpl repository;
+  late AuthController authController;
+  late ProfileController profileController;
+  late FirebaseMock firebaseAuth;
+  late UserCredentialMock userCredential;
+  late UserMock user;
+
+  setUp(() {
+    firebaseAuth = FirebaseMock();
+    repository = AuthRepositoryImpl(firebaseAuth);
+    authController = AuthController(authRepository: repository);
+    profileController = ProfileController(authRepository: repository);
+    userCredential = UserCredentialMock();
+    user = UserMock();
+  });
+
+  test('profile sign out test', () async {
+    expect(profileController.state, isA<ProfileInitialState>());
+    when(
+      () => firebaseAuth.signInWithEmailAndPassword(
+          email: 'user@email.com', password: 'user@123'),
+    ).thenAnswer((invocation) async => userCredential);
+    when((() => userCredential.user)).thenReturn(user);
+    when(
+      () => firebaseAuth.signOut(),
+    ).thenAnswer((invocation) async => user);
+    await profileController.signOut();
+    expect(profileController.state, isA<ProfileLogoutState>());
+  });
+}
