3612eb6341c9c075bceee5ed8e46bbee9faba966
josemailson <jmailsons@gmail.com>
2023-01-25 18:39:35 -0300

    função para atualizar valor de balanço final

--- lib/presentation/home/widgets/current_balance.dart
+++ lib/presentation/home/widgets/current_balance.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import '../../../resources/strings.dart';
+import 'package:intl/intl.dart';
+import '../../../data/models/transactions_model.dart';
+import '../../../locator.dart';
+import '../../../resources/colors.dart';
 import '../../../resources/text_style.dart';
+import '../controller/transactions_controller.dart';
+import '../controller/transactions_state.dart';
 
 class CurrentBalance extends StatefulWidget {
   const CurrentBalance({
@@ -12,11 +17,56 @@ class CurrentBalance extends StatefulWidget {
 }
 
 class _CurrentBalanceState extends State<CurrentBalance> {
+  final transactionsController = TransactionsController(
+      authRepository: getIt(), transactionsRepository: getIt());
+
+  @override
+  void initState() {
+    super.initState();
+    transactionsController.getTransactionsList();
+  }
+
+  String getCurrentBalance(List<TransactionsModel> transactionsList) {
+    late double totalIcome = 0;
+    late double totalExpense = 0;
+    var formatter = NumberFormat.currency(locale: 'pt-BR', symbol: 'R\$');
+    for (var transaction in transactionsList) {
+      if (transaction.type == 'Receita') {
+        totalIcome += transaction.value;
+      } else if (transaction.type == 'Despesa') {
+        totalExpense += transaction.value;
+      }
+    }
+    final currentBalance = formatter.format(totalIcome - totalExpense);
+    return currentBalance;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      Strings.currentBalance,
-      style: AppTextStyles.currentBalance,
-    );
+    return Builder(builder: (context) {
+      return ValueListenableBuilder<TransactionState>(
+        valueListenable: transactionsController,
+        builder: (_, state, __) {
+          if (state is TransactionLoadingState) {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: AppColors.blueVibrant,
+            ));
+          }
+          if (state is TransactionSuccessState) {
+            return Text(
+              getCurrentBalance(state.transactionListModel),
+              style: AppTextStyles.currentBalance,
+            );
+          }
+          if (state is TransactionErrorState) {
+            return Center(
+              child: Text(state.message),
+            );
+          }
+          return Container();
+        },
+      );
+    });
   }
 }
