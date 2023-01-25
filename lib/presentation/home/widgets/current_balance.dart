c06a8d4ebb25300c0f2c187820eacb3039881daa
josemailson <jmailsons@gmail.com>
2023-01-24 21:26:46 -0300

    atualizar o saldo total na home

--- lib/presentation/home/widgets/current_balance.dart
+++ lib/presentation/home/widgets/current_balance.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import '../../../resources/strings.dart';
 import '../../../resources/text_style.dart';
 
-class CurrentBalance extends StatelessWidget {
+class CurrentBalance extends StatefulWidget {
   const CurrentBalance({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<CurrentBalance> createState() => _CurrentBalanceState();
+}
+
+class _CurrentBalanceState extends State<CurrentBalance> {
   @override
   Widget build(BuildContext context) {
     return const Text(
