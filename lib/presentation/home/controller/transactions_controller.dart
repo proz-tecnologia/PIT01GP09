b5cc08bcca0a5141bd5c892f08b4034bf4a37425
cindi_star <cindi_nishi@hotmail.com>
2023-01-26 16:01:12 -0300

    refatorado o transactions_controller.dart

--- lib/presentation/home/controller/transactions_controller.dart
+++ lib/presentation/home/controller/transactions_controller.dart
@@ -1,85 +1,26 @@
-import 'package:finance_app/data/models/datetime_model.dart';
-import 'package:finance_app/data/models/transactions_model.dart';
 import 'package:finance_app/domain/repositories/authentication/auth_repository.dart';
 import 'package:finance_app/domain/repositories/transactions/transactions_repository.dart';
 import 'package:finance_app/presentation/home/controller/transactions_state.dart';
+import 'package:finance_app/presentation/home/controller/transactions_state_initial.dart';
+import 'package:finance_app/presentation/home/controller/transactions_state_success.dart';
 import 'package:flutter/material.dart';
 
-class TransactionsController extends ValueNotifier<TransactionState> {
-  final TransactionsRepository _transactionsRepository;
-  final AuthRepository _authRepository;
+class TransactionsController {
+  final TransactionsRepository transactionsRepository;
+  final AuthRepository authRepository;
 
   TransactionsController({
-    required TransactionsRepository transactionsRepository,
-    required AuthRepository authRepository,
-  })  : _transactionsRepository = transactionsRepository,
-        _authRepository = authRepository,
-        super(TransactionInitialState());
+    required this.transactionsRepository,
+    required this.authRepository,
+  });
 
-  int _monthFilter = 0;
-  int _yearFilter = 0;
+  final ValueNotifier<TransactionState> state =
+      ValueNotifier<TransactionState>(TransactionInitialState());
 
-  List<TransactionsModel> _transactionsList = [];
-
-  List<TransactionsModel> get transactionsList {
-    if (_monthFilter == 0) {
-      return _transactionsList;
-    } else {
-      return _transactionsList
-          .where((transactions) =>
-              transactions.isAtMonthYear(_monthFilter, _yearFilter))
-          .toList();
-    }
-  }
-
-  void setTransactionsList(List<TransactionsModel> transactionsList) {
-    _transactionsList = transactionsList;
-  }
-
-  void filterTransactionList(int month, int year) {
-    _monthFilter = month;
-    _yearFilter = year;
-  }
-
-  void fetchTransactionListFromRepository() {
-    final userId = _authRepository.currentUser?.uid;
-    _transactionsRepository
-        .getTransactionsList(userId ?? "")
-        .then((value) => setTransactionsList(value));
-  }
-
-  List<DateTimeModel> get monthsList {
-    Map<String, DateTimeModel> list = {};
-    DateTimeModel model = DateTimeModel(date: null);
-    list.putIfAbsent(model.name, () => model);
-    for (TransactionsModel transaction in _transactionsList) {
-      DateTimeModel model = DateTimeModel(date: transaction.date);
-      list.putIfAbsent(model.name, () => model);
-    }
-    return list.values.toList()
-      ..sort((a, b) {
-        if (a.date == null) {
-          return -1;
-        }
-        if (b.date == null) {
-          return 1;
-        }
-        return a.date!.compareTo(b.date!);
-      });
-  }
-
-  Future<void> getTransactionsList() async {
-    value = TransactionLoadingState();
-
-    try {
-      final userId = _authRepository.currentUser?.uid;
-
-      final transactionsList =
-          await _transactionsRepository.getTransactionsList(userId ?? "");
-
-      value = TransactionSuccessState(transactionsList);
-    } catch (e) {
-      value = TransactionErrorState(e.toString());
-    }
+  void fetchTransactions() {
+    final userId = authRepository.currentUser?.uid;
+    transactionsRepository.getTransactionsList(userId ?? "").then((list) {
+      state.value = TransactionStateSuccess(transactions: list);
+    });
   }
 }
