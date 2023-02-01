49f7cc8249dbc61877fd86a2fe0a55b187c5eef0
cindi_star <cindi_nishi@hotmail.com>
2023-01-31 21:29:45 -0300

    ajustada a AddIncomePage

--- lib/presentation/income/page/add_income_page.dart
+++ lib/presentation/income/page/add_income_page.dart
@@ -1,3 +1,4 @@
+import 'package:finance_app/data/models/transactions_model.dart';
 import 'package:finance_app/resources/colors.dart';
 import 'package:finance_app/resources/shared_widgets/add_new_transaction_widget.dart';
 import 'package:finance_app/utils/list_income_util.dart';
@@ -5,21 +6,18 @@ import 'package:flutter/material.dart';
 
 import '../../../resources/strings.dart';
 
-class AddIncomePage extends StatefulWidget {
-  const AddIncomePage({super.key});
+class AddIncomePage extends StatelessWidget {
+  final TransactionsModel? transaction;
+  const AddIncomePage({super.key, required this.transaction});
 
-  @override
-  State<AddIncomePage> createState() => _AddIncomePageState();
-}
-
-class _AddIncomePageState extends State<AddIncomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
         child: AddNewTransactionWidget(
       color: AppColors.greenVibrant,
       type: Strings.income,
       list: listIncome,
+      transaction: transaction,
     ));
   }
 }
