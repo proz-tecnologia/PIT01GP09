8181fd0163429ee559af61b023d696a57f9630ed
cindi_star <cindi_nishi@hotmail.com>
2023-01-31 23:16:01 -0300

    modificado o UpdateTransactionController

--- lib/presentation/transactions/controller/update_transaction_controller.dart
+++ lib/presentation/transactions/controller/update_transaction_controller.dart
@@ -1,24 +1,41 @@
 import 'package:finance_app/data/models/transactions_model.dart';
+import 'package:finance_app/domain/repositories/authentication/auth_repository.dart';
 import 'package:finance_app/domain/repositories/transactions/transactions_repository.dart';
 import 'package:finance_app/presentation/transactions/controller/update_state.dart';
 import 'package:flutter/material.dart';
 
 class UpdateTransactionController {
   final TransactionsRepository transactionsRepository;
+  final AuthRepository authRepository;
 
   UpdateTransactionController({
+    required this.authRepository,
     required this.transactionsRepository,
   });
 
-  final ValueNotifier<UpdateState> state =
-      ValueNotifier<UpdateState>(UpdateInitialState());
+  final notifier = ValueNotifier<UpdateState>(UpdateInitialState());
+
+  UpdateState get state => notifier.value;
 
   Future<void> updateTransaction(TransactionsModel transaction) async {
+    notifier.value = UpdateLoadingState();
+
     try {
-      await transactionsRepository.updateTransaction(transaction);
-      state.value = UpdateSuccessState();
+      final userId = authRepository.currentUser?.uid;
+
+      final transactionModel = TransactionsModel(
+          description: transaction.description,
+          category: transaction.category,
+          type: transaction.type,
+          value: transaction.value,
+          date: transaction.date,
+          userId: userId ?? "");
+
+      await transactionsRepository.updateTransaction(transactionModel);
+
+      notifier.value = UpdateSuccessState();
     } catch (e) {
-      state.value = UpdateErrorState('Erro ao atualizar!');
+      notifier.value = UpdateErrorState(e.toString());
     }
   }
 }
